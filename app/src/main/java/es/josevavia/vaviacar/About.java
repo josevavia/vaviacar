b69b6b8d81ba9eb4f54bc74e4455dcdc8d4a277c
Jose Vázquez Viader <jviader@confirmsign.com>
2014-07-04 00:35:15 +0200

    Added about activity

---
+++ app/src/main/java/es/josevavia/vaviacar/About.java
@@ -0,0 +1,17 @@
+package es.josevavia.vaviacar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import es.josevavia.vaviacar.R;
+
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
